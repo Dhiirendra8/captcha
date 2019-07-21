400e6747ea0758a1cfaeedc2a97c04fbb3114690
Dhirendra Singh <dhiirendra8@gmail.com>
2019-07-21 19:29:45 +0530

    Remove duplicated directory

--- spring-boot-rest-3/src/main/java/com/test/main/model/Vehicle.java
+++
@@ -1,7 +0,0 @@
-package com.test.main.model;
-
-public interface Vehicle {
-
-	public String run();
-	public String color();
-}
--- spring-boot-rest-3/src/main/java/com/test/main/topic/TopicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test.main.topic;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, String>{
-
-}
